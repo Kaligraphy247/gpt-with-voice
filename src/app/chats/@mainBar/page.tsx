b0d9976efeadf3c1f97c2a336d52d2137c67c4ae
kaligraphy247 <chikeziejames3@gmail.com>
2023-07-29 22:50:15 +0200

    fix for last commit

--- src/app/chats/@mainBar/page.tsx
+++ src/app/chats/@mainBar/page.tsx
@@ -1,7 +1,7 @@
 export default async function MainBar() {
   // await new Promise((resolve) => setTimeout(resolve, 2_000));
 
-    return <div className="border w-full bg-gray-800  rounded min-h-full"></div>;
+    return <div className="border w-full bg-gray-50 dark:bg-gray-800  rounded min-h-full"></div>;
 //   return <></>;
 }
 
--- src/app/chats/layout.tsx
+++ src/app/chats/layout.tsx
@@ -7,10 +7,10 @@ export default function ChatsLayout(props: {
     <>
       {props.children}
       <div className="border-0 bg flex">
-        <div className="hidden md:w-3/5 md:flex 2xl:w-1/4 xl:w-1/3 bg-gray-950 border mr-6 px-2 py-1 rounded h-[90vh]">
+        <div className="hidden md:w-3/5 md:flex 2xl:w-1/4 xl:w-1/3 bg-gray-50 border mr-6 px-2 py-1 rounded h-[90vh] dark:bg-gray-950">
           {props.sidebar}
         </div>
-        <div className="border w-full bg p-0.5 rounded">
+        <div className="border w-full p-0.5 rounded">
           {props.mainBar}
         </div>
       </div>
